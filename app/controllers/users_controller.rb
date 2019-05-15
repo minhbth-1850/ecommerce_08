364cb564508d814b087ff7a02a8df9239cd08009
minhbth-1850 <tran.hoang.minhb@sun-asterisk@.com>
2019-05-15 13:48:51 +0700

    sign_in, sign_out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,23 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: %i(new show create)
+  before_action :load_user, except: %i(new index create)
+  before_action :correct_user, only: :edit
+
   def new
     @user = User.new
   end
 
-  def show
-    @user = User.find_by id: params[:id]
-    return @user if @user
-
-    flash[:danger] = t "message.nil_user"
-    redirect_to root_path
+  def index
+    @users = User.paginate page: params[:page],
+      per_page: Settings.users.per_page
   end
+
+  def show; end
 
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = t "label.welcome", logo: t("link.logo")
       redirect_to @user
     else
@@ -21,10 +25,32 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
   private
 
   def user_params
     params.require(:user).permit :name, :email,
       :password, :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "message.login_plz"
+    redirect_to login_path
+  end
+
+  def correct_user
+    redirect_to root_path unless current_user? @user
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return @user if @user
+
+    flash[:danger] = t "message.nil_user"
+    redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   enum role: {customer: 0, admin: 1}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  attr_accessor :remember_token
   validates :email, presence: true,
     length: {maximum: Settings.users.email_length},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
@@ -9,4 +10,35 @@ class User < ApplicationRecord
     length: {minimum: Settings.users.pass_min_length}
   before_save{email.downcase!}
   has_secure_password
+
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create string, cost: cost
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
+
+  def admin?
+    false
+  end
 end
