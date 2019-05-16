39536ce2f804e57d96d7a45a99cca6e8390d21d4
minhbth-1850 <tran.hoang.minhb@sun-asterisk@.com>
2019-05-16 09:58:11 +0700

    update profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,15 @@ class UsersController < ApplicationController
 
   def edit; end
 
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "message.profile_update"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
