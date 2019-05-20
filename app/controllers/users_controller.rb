73fbd828370758a032635b781f8ba0bcc1b962f0
minhbth-1850 <tran.hoang.minhb@sun-asterisk@.com>
2019-05-20 10:22:31 +0700

    signup user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    return @user if @user
+
+    flash[:danger] = t "flash.nil_object", name: "User"
+    redirect_to root_path
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "label.welcome", logo: t("logo")
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :phone, :address,
+      :password, :password_confirmation
+  end
+end
