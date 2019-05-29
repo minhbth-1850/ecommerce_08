b7bc51317b086fe1e5a2635150665d2205a003fa
minhbth-1850 <tran.hoang.minhb@sun-asterisk@.com>
2019-05-29 09:22:51 +0700

    add plugin

---
+++ app/assets/javascripts/vchat.js
@@ -0,0 +1,10 @@
+var _vc_data = {id : 7107704, secret : '0c254df8df731a67f37167bf6d8efae7'};
+document.addEventListener("turbolinks:load", function() {
+(function() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';ga.async=true;
+  ga.defer=true;ga.src = '//live.vnpgroup.net/client/tracking.js?id=7107704';
+  var s = document.getElementsByTagName('script');
+  s[0].parentNode.insertBefore(ga, s[0]);
+})();
+})
